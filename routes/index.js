c57b457f254d08c9d8c55c3b18db64f64c988dcf
warren-west <teacher.warren.english@gmail.com>
2026-01-16 13:24:07 +0100

    fix: Navbar on homepage shows login / logout correctly.

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', {
      title: 'Express', 
-     message: "Hello from the server" 
+     message: "Hello from the server",
+     currentUser: req.user || undefined
     });
 });
 
